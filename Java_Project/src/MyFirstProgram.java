6aee11164e09636a104ffc2a61d29ff5c37fd31a
Isaac Pinedo <81377644+IsaacPinedo@users.noreply.github.com>
2025-05-13 22:14:52 -0600

    Add files via upload

---
+++ Java_Project/src/MyFirstProgram.java
@@ -0,0 +1,6 @@
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        System.out.println("I'll make a lot of money with this, I hope in God");
+        System.out.println("Lucas, I miss you everyday!");
+    }
+}
